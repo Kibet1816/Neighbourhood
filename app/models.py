92328f637abc14dd9df853cf2a02c30f540d652c
Denis Kibet <kibet1816@gmail.com>
2019-08-12 17:20:50 +0300

    Create profile model class and in models.py and migrate the change

--- app/models.py
+++ app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Profile(models.Model):
+    photo = models.ImageField(upload_to = 'media/')
+    bio = models.TextField(default="Hey there!I\'m using Instagram")
+    user = models.OneToOneField(User,on_delete=models.CASCADE, primary_key=True)
