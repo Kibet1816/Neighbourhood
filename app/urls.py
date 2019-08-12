0c3c9a4f56030eba26d4d4ef5ebb8ba485c75bca
Denis Kibet <kibet1816@gmail.com>
2019-08-12 14:40:59 +0300

    Add login form in login.html file

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'',views.index,name='index'),
-    url(r'profile/',views.profile,name='prof'),
+    url('^$',views.index,name='index'),
+    url('^profile/$',views.profile,name='prof'),
 ]
