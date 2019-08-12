81a96d32b9f7ab9f9a6cf4ac6edebcbeb0398f24
Denis Kibet <kibet1816@gmail.com>
2019-08-12 17:41:39 +0300

    Add location and name fields to profile model class in models.py

--- app/models.py
+++ app/models.py
@@ -6,4 +6,6 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     photo = models.ImageField(upload_to = 'media/')
     bio = models.TextField(default="Hey there!I\'m using Instagram")
+    location = models.CharField(max_length = 60,default='Somewhere')
+    neighbourhood_name = models.CharField(max_length=20,default='Nameless')
     user = models.OneToOneField(User,on_delete=models.CASCADE, primary_key=True)
