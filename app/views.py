bed91fcd5ba54423ec7e087199c9fa5d8f3944c8
Denis Kibet <kibet1816@gmail.com>
2019-08-12 17:33:51 +0300

    Add route for users to edit profile in urls.py file

--- app/views.py
+++ app/views.py
@@ -1,11 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth.decorators import login_required
+from .models import *
 
 # Create your views here.
 def index(request):
     """
     View function to render the homepage
     """
+    try:
+        current_user=request.user
+        profile =Profile.objects.get(user=current_user)
+    except ObjectDoesNotExist:
+        return redirect('edit')
     return render(request,"all-templates/index.html")
 
 @login_required(login_url='/accounts/login/')
@@ -14,3 +21,4 @@ def profile(request):
     View function to render the homepage
     """
     return render(request,"all-templates/profile.html")
+
